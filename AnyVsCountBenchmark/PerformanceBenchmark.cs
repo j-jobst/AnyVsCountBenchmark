f2aaf4ab847a04275b564d78487e3b02b0b02d24
Jannick Jobst <jannick.jobst@dedalus.com>
2024-02-20 18:41:40 +0100

    Initial commit

---
+++ AnyVsCountBenchmark/PerformanceBenchmark.cs
@@ -0,0 +1,65 @@
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace AnyVsCountBenchmark;
+
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+public class PerformanceBenchmark
+{
+    private static readonly IEnumerable<int> _numbersEnumerable = Enumerable.Range(1, 50000);
+    private static readonly ICollection<int> _numbersCollection = Enumerable.Range(1, 50000).ToList();
+
+    [Benchmark]
+    public bool CheckEnumerableWithAny()
+    {
+        return _numbersEnumerable.Any();
+    }
+    
+    [Benchmark]
+    public bool CheckEnumerableWithAnyAndCondition()
+    {
+        return _numbersEnumerable.Any(x => x > 500);
+    }
+    
+    [Benchmark]
+    public bool CheckEnumerableWithCount()
+    {
+        return _numbersEnumerable.Count() > 0;
+    }
+    
+    [Benchmark]
+    public bool CheckEnumerableWithCountAndCondition()
+    {
+        return _numbersEnumerable.Count(x => x > 500) > 0;
+    }
+    
+    [Benchmark]
+    public bool CheckCollectionWithAny()
+    {
+        return _numbersEnumerable.Any();
+    }
+    
+    [Benchmark]
+    public bool CheckCollectionWithAnyAndCondition()
+    {
+        return _numbersEnumerable.Any(x => x > 500);
+    }
+    
+    [Benchmark]
+    public bool CheckCollectionWithCount()
+    {
+        return _numbersEnumerable.Count() > 0;
+    }
+    
+    [Benchmark]
+    public bool CheckCollectionWithCountAndCondition()
+    {
+        return _numbersEnumerable.Count(x => x > 500) > 0;
+    }
+    
+    [Benchmark]
+    public bool CheckCollectionWithCountProperty()
+    {
+        return _numbersCollection.Count > 0;
+    }
+}
